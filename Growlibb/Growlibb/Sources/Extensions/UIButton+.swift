b1061459e2a0bb1898bb5d487a11e5d617835d98
Yuri Lee <37764504+yurrrri@users.noreply.github.com>
2023-01-09 01:52:09 +0900

    ⚡약관 동의, 홈 - 달력 이동 버튼 터치영역 확장

--- Growlibb/Growlibb/Sources/Extensions/UIButton+.swift
+++ Growlibb/Growlibb/Sources/Extensions/UIButton+.swift
@@ -12,4 +12,9 @@ extension UIButton {
         self.clipsToBounds = true
         self.layer.cornerRadius = 9
     }
+    
+    open override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+      let hitArea = self.bounds.insetBy(dx: -20, dy: -20)
+      return hitArea.contains(point)
+    }
 }
